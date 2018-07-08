d5dea56c1f4c7bd5665abd91ef8a984526d3f8c0
valdiss <valentin.dissais@gmail.com>
2018-07-08 15:21:46 +0200

    buttons confirm, cancel

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,24 +1,30 @@
-import React, { Fragment } from 'react';
+import React, { Fragment } from "react";
+import Button from "@material-ui/core/Button";
 
 const orderSummary = props => {
-    const ingredientSummary = Object.keys(props.ingredients).map(igKey => {
-        return (
-            <li key={igKey}>
-                <span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {props.ingredients[igKey]}
-            </li>
-        );
-    });
-
+  const ingredientSummary = Object.keys(props.ingredients).map(igKey => {
     return (
-            <Fragment>
-                <h2 style={{ textAlign: 'center' }}>Your Order</h2>
-                <p>A Delicious burger with the following ingredients:</p>
-                <ul>
-                    {ingredientSummary}
-                </ul>
-                <p>Proceed to checkout?</p>
-            </Fragment>
-    )
+      <li key={igKey}>
+        <span style={{ textTransform: "capitalize" }}>{igKey}</span>:{" "}
+        {props.ingredients[igKey]}
+      </li>
+    );
+  });
+
+  return (
+    <Fragment>
+      <h2 style={{ textAlign: "center" }}>Your Order</h2>
+      <p>A Delicious burger with the following ingredients:</p>
+      <ul>{ingredientSummary}</ul>
+      <p>Proceed to checkout?</p>
+      <Button size="medium" variant="contained" color="secondary">
+        Cancel
+      </Button>
+      <Button size="medium" variant="contained" color="primary">
+        Confirm
+      </Button>
+    </Fragment>
+  );
 };
 
 export default orderSummary;
